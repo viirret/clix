79bc2dce85394b8868c6b645f42a96b5bd32ca36
valtteriviirret <viirretvaltteri@gmail.com>
2022-07-08 22:21:21 +0300

    remove hh

--- examples/clickerGame.cc
+++ examples/clickerGame.cc
@@ -1,140 +1,158 @@
-#include "clickerGame.hh"
+#include "../clix/Core.hh"
+#include "../clix/Player.hh"
+#include "../clix/Crosshair.hh"
 
-clickerGame::clickerGame() 
-	: 	Core(),			// start core
-		crosshair(),	// implement default crosshair
-		player(),		// add player
-		resetButton((glb::assetsPath + "playagain.png").c_str(), Vec2f(1, 1)) // add resetbutton for game over
+class clickerGame : public Core
 {
-	resetGame();
-	spawnEnemyRandom("arch.png");
-}
-
-void clickerGame::update()
-{
-	Core::update();
-	updateGame();
-	draw();
-}
-
-void clickerGame::updateGame()
-{
-	// setup for game over
-	if(player.gun.getCapacity() <= 0)
-		isGameOver = true;
-
-	if(isGameOver)
-		gameOver();
-
-	// update crosshair
-	crosshair.updateCrosshair(mousePosition);
-
-	if(IsMouseButtonPressed(0))
-	{
-		if(player.gun.getCapacity() > 0)
+	public:
+		clickerGame() :
+		 	Core(),			// start core
+			crosshair(),	// implement default crosshair
+			player(),		// add player
+			resetButton((glb::assetsPath + "playagain.png").c_str(), Vec2f(1, 1)) // add resetbutton for game over
 		{
-			audio.playSound("gunshow.mp3");
-
-			// update mag size
-			player.gun.fire();
-		}
-	}
-
-	// GAMEPLAY LOGIC	
-	for(size_t i = 0; i < (size_t)objects.size(); i++)
-	{
-		// if enemy is clicked
-		if(objects[i]->checkHit(currentClick))
-		{
-			// redefine currenthit
-			 currentClick = Vec2f();
-
-			switch(objects[i]->type)
-			{
-				case Object::Type::enemy: 
-					// update amount of killed enemies
-					killedEnemies++;
-					killedString = std::to_string(killedEnemies);
-					break;
-				case Object::Type::ammo:
-					player.gun.restock();
-					break;
-				case Object::Type::object: break;
-			}
-			
-			// delete clicked element
-			objects.erase(objects.begin() + i);
-
-			// add new enemy 
+			resetGame();
 			spawnEnemyRandom("arch.png");
-
-			objects[0]->setRandomTarget();
 		}
-	}
 
-	// OBJECT MOVEMENT LOGIC
-	for(auto& obj : objects)
-	{
-		switch(obj->type)
+		void update()
 		{
-			case Object::Type::enemy: 
-				obj->moveTowardsTarget();
-				break;
-
-			case Object::Type::ammo: break;
-			case Object::Type::object: break;
+			Core::update();
+			updateGame();
+			draw();
 		}
-	}
-}
 
-void clickerGame::gameOver() 
-{
-	// reset current click to (0, 0) to avoid instant hit
-	if(!currentHitChanged)
-	{
-		currentClick = Vec2f();
-		currentHitChanged = true;
-	}
+		void updateGame()
+		{
+			// setup for game over
+			if(player.gun.getCapacity() <= 0)
+				isGameOver = true;
 
-	// render things
-	DrawText("clickerGame Over!", GetScreenWidth() / 3, GetScreenHeight() / 3, 40, BLACK);
-	resetButton.draw(WHITE);
+			if(isGameOver)
+				gameOver();
 
-	// is resetbutton is clicked start a new game
-	if(resetButton.checkHit(currentClick))
-	{
-		resetGame();
-		isGameOver = false;
-	}
-}
+			// update crosshair
+			crosshair.updateCrosshair(mousePosition);
 
-void clickerGame::resetGame()
-{
-	killedEnemies = 0;
-	killedString = std::to_string(killedEnemies);
-	player.gun.restock();
-}
+			if(IsMouseButtonPressed(0))
+			{
+				if(player.gun.getCapacity() > 0)
+				{
+					audio.playSound("gunshow.mp3");
 
-void clickerGame::draw()
-{
-	BeginDrawing();
-	ClearBackground(WHITE);
+					// update mag size
+					player.gun.fire();
+				}
+			}
 
-	crosshair.draw(WHITE);
+			// GAMEPLAY LOGIC	
+			for(size_t i = 0; i < (size_t)objects.size(); i++)
+			{
+				// if enemy is clicked
+				if(objects[i]->checkHit(currentClick))
+				{
+					// redefine currenthit
+					 currentClick = Vec2f();
 
-	// render enemies
-	for(auto& obj : objects)
-		obj->draw(WHITE);
+					switch(objects[i]->type)
+					{
+						case Object::Type::enemy: 
+							// update amount of killed enemies
+							killedEnemies++;
+							killedString = std::to_string(killedEnemies);
+							break;
+						case Object::Type::ammo:
+							player.gun.restock();
+							break;
+						case Object::Type::object: break;
+					}
+					
+					// delete clicked element
+					objects.erase(objects.begin() + i);
 
-	// update ammo count
-	std::string ammoText = player.gun.capacity + " / " + player.gun.magazineSize;
-	DrawText(ammoText.c_str(), GetScreenWidth() - GetScreenWidth() / 4, GetScreenHeight() - GetScreenHeight() / 10, 30, BLUE);
+					// add new enemy 
+					spawnEnemyRandom("arch.png");
 
-	// update killed enemies count
-	DrawText(killedString.c_str(), GetScreenWidth() / 20, GetScreenHeight() / 20, 30, BLACK);
+					objects[0]->setRandomTarget();
+				}
+			}
 
-	EndDrawing();
-}
+			// OBJECT MOVEMENT LOGIC
+			for(auto& obj : objects)
+			{
+				switch(obj->type)
+				{
+					case Object::Type::enemy: 
+						obj->moveTowardsTarget();
+						break;
+
+					case Object::Type::ammo: break;
+					case Object::Type::object: break;
+				}
+			}
+		}
+
+		void gameOver() 
+		{
+			// reset current click to (0, 0) to avoid instant hit
+			if(!currentHitChanged)
+			{
+				currentClick = Vec2f();
+				currentHitChanged = true;
+			}
+
+			// render things
+			DrawText("clickerGame Over!", GetScreenWidth() / 3, GetScreenHeight() / 3, 40, BLACK);
+			resetButton.draw(WHITE);
+
+			// is resetbutton is clicked start a new game
+			if(resetButton.checkHit(currentClick))
+			{
+				resetGame();
+				isGameOver = false;
+			}
+		}
+
+		void resetGame()
+		{
+			killedEnemies = 0;
+			killedString = std::to_string(killedEnemies);
+			player.gun.restock();
+		}
+
+		void draw()
+		{
+			BeginDrawing();
+			ClearBackground(WHITE);
+
+			crosshair.draw(WHITE);
+
+			// render enemies
+			for(auto& obj : objects)
+				obj->draw(WHITE);
+
+			// update ammo count
+			std::string ammoText = player.gun.capacity + " / " + player.gun.magazineSize;
+			DrawText(ammoText.c_str(), GetScreenWidth() - GetScreenWidth() / 4, GetScreenHeight() - GetScreenHeight() / 10, 30, BLUE);
+
+			// update killed enemies count
+			DrawText(killedString.c_str(), GetScreenWidth() / 20, GetScreenHeight() / 20, 30, BLACK);
+
+			EndDrawing();
+		}
+
+	private:
+		// instances of other classes
+		Crosshair crosshair;
+		Player player;
+		Object resetButton;
+
+		// variables for gameplay
+		int killedEnemies;
+		std::string killedString;
+		bool isGameOver = false;
+		bool currentHitChanged = false;
+};
 
 int main(int argc, char** argv)
 {
