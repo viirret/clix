776665d24fac28bfcd823d28646512755a5b4148
valtteriviirret <viirretvaltteri@gmail.com>
2022-08-08 05:08:39 +0300

    movement

--- examples/snake.cc
+++ examples/snake.cc
@@ -1,15 +1,48 @@
 #include "../clix/Core.hh"
+#include "../clix/Controls.hh"
 
+// blockSize depends on screen's value so it's defined after Core()
 Vec2f blockSize;
 
 class Snake
 {
 	public:
-		Snake()
+		void initialize()
 		{
-			for(int i = 0; i < 3; i++)
+			for(size_t i = 0; i < (size_t)initialAmount; i++)
 			{
-				addBlock(Vec2f(i * blockSize.x, 0));
+				addBlock(Vec2f(i * blockSize.x + GetScreenWidth(), (float)GetScreenHeight()));
+			}
+		}
+
+		void setDirection()
+		{
+			if(Controls::up()) direction = 1;
+			if(Controls::down()) direction = 2;
+			if(Controls::left()) direction = 3;
+			if(Controls::right()) direction = 4;
+		}
+
+		void move()
+		{
+			Vec2f oldBlock = blocks[0];
+
+			switch(direction)
+			{
+				case 1: blocks[0].y -= blockSize.y; break;
+				case 2: blocks[0].y += blockSize.y; break;
+				case 3: blocks[0].x -= blockSize.x; break;
+				case 4: blocks[0].x += blockSize.x; break;
+			}
+
+			for(size_t i = 0; i < (size_t)blocks.size(); i++)
+			{
+				if(i != 0)
+				{
+					Vec2f newBlock = blocks[i];
+					blocks[i] = oldBlock;
+					oldBlock = newBlock;
+				}
 			}
 		}
 
@@ -21,6 +54,11 @@ class Snake
 		double speed = 2.0;
 		int size = 3;
 		Color color = RED;
+		Color secondColor = BLACK;
+		int initialAmount = 5;
+
+		// the initial direction of the snake (1..4)
+		int direction = 3;
 
 		std::vector<Vec2f> blocks;
 };
@@ -31,20 +69,29 @@ class Game : public Core
 		Game() : Core()
 		{
 			resize();
+			s.initialize();
 		}
 
 		void update() override
 		{
 			Core::update();
 
-			for(auto& b : s.blocks)
+			s.setDirection();
+
+			if(timer > s.speed)
 			{
-				drawRectangle(b, blockSize, s.color);
+				s.move();
+				timer = 0.0;
+			}
+			timer += delta;
+
+			for(size_t i = 0; i < (size_t)s.blocks.size(); i++)
+			{
+				drawRectangle(s.blocks[i], blockSize, (i == 0) ? s.secondColor : s.color);
 			}
 
 			if(screenResized)
 				resize();
-
 		}
 
 		void resize()
@@ -53,6 +100,7 @@ class Game : public Core
 		}
 	private:
 		Snake s;
+		double timer;
 };
 
 int main(int argc, char** argv)
